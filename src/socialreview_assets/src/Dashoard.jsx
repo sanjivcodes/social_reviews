2e878da2b423e7493944efffc61f50c3bf15192f
Sanjiv <sanjivcodes@gmail.com>
2022-06-22 00:05:22 +0530

    Review section

--- src/socialreview_assets/src/Dashoard.jsx
+++ src/socialreview_assets/src/Dashoard.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react"
 import logo from "../assets/logo-dark.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export function Dashboard() {
   return (
@@ -9,7 +11,7 @@ export function Dashboard() {
   <div class="container py-5">
     <div class="row d-flex justify-content-center">
       <div class="col-md-10 col-xl-8 text-center">
-        <h3 class="fw-bold mb-4">Testimonials</h3>
+        <h3 class="fw-bold mb-4">Social Reviews</h3>
         <p class="mb-4 pb-2 mb-md-5 pb-md-0">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet
           numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum
@@ -19,30 +21,26 @@ export function Dashboard() {
     </div>
 
     <div class="row text-center">
-      <div class="col-md-4 mb-4 mb-md-0">
-        <div class="card">
+      <div class="col-md-12 mb-4 mb-md-0">
+        <div class="card shadow-md">
           <div class="card-body py-4 mt-2">
-            <div class="d-flex justify-content-center mb-4">
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp"
-                class="rounded-circle shadow-1-strong" width="100" height="100" />
-            </div>
-            <h5 class="font-weight-bold">Teresa May</h5>
-            <h6 class="font-weight-bold my-3">Founder at ET Company</h6>
+            
+            
             <ul class="list-unstyled d-flex justify-content-center">
               <li>
-                <i class="fas fa-star fa-sm text-info"></i>
+              <FontAwesomeIcon icon={faStar} size="sm" className="text-warning"/>
               </li>
               <li>
-                <i class="fas fa-star fa-sm text-info"></i>
+              <FontAwesomeIcon icon={faStar} size="sm" className="text-warning"/>
               </li>
               <li>
-                <i class="fas fa-star fa-sm text-info"></i>
+                <FontAwesomeIcon icon={faStar} size="sm" className="text-warning"/>
               </li>
               <li>
-                <i class="fas fa-star fa-sm text-info"></i>
+                <FontAwesomeIcon icon={faStar} size="sm" className="text-warning"/>
               </li>
               <li>
-                <i class="fas fa-star-half-alt fa-sm text-info"></i>
+              <FontAwesomeIcon icon={faStar} size="sm" className="text-secondary"/>
               </li>
             </ul>
             <p class="mb-2">
@@ -50,6 +48,7 @@ export function Dashboard() {
               consectetur adipisicing elit. Quod eos id officiis hic tenetur quae quaerat
               ad velit ab hic tenetur.
             </p>
+            <h6 class="font-weight-bold my-3">Teresa</h6>
           </div>
         </div>
       </div>
